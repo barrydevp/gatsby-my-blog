8de7a41472bf2e0295ad4d2e9ecbf0f397f95271
barrydevp <barrydevp@gmail.com>
2019-12-05 20:59:28 +0700

    create post 'how i fight with gatsby'

--- content/blog/who-am-i/index.md
+++ content/blog/who-am-i/index.md
@@ -1,11 +1,11 @@
 ---
-title: Tôi là ai?
+title: Mình là ai?
 date: "2019-12-05T16:12:03.284Z"
 description: "Đào Minh Hải(barydevp) sinh viên trường Đại Học Công Nghệ - Đại Học Quốc Gia Hà Nội."
 ---
 
-# Mình là ai :) ?
-##### Chào các bạn mình tên là Đào Minh Hải (barry), là tác giả của Blog này :D.
+# Mở đầu
+#### Chào các bạn mình tên là Đào Minh Hải (barry), là tác giả của Blog này :D.
 Tại thời điểm này mình 19 tuổi. Là sinh viên trường Đại Học Công Nghệ - Đại Học Quốc Gia Hà Nội. Chuyên nghành chính của mình là Công Nghệ Thông Tin. Hiện tại mình đang học trên trường và đi làm tại một công ty ở Hà Nội. Công việc chính của mình tại công ty là một Back End Developer (Nodejs/Expressjs, GraphQL, Redis, MongoDB, MySQL, Firebase, Facebook API) :D.
 
 # Về Blog này
@@ -17,4 +17,4 @@ Blog này mình cũng muốn là nơi lưu giữ những kỷ niệm để khi n
 Thời điểm mình viết blog này là vào khoảng cuối học kỳ 1 năm 2 đại học, khi ấy các môn trên trường hầu như đã hoàn thành và đang trong thời gian ôn luyện để chuẩn bị thi kết thúc môn, cũng là lúc mà mình mới bị tai nạn xe máy(nhẹ) vào tuần trước, ở nhà không biết làm gì nên đành vọc thử lại React. Trong thời gian này mình tìm hiểu được khá nhiều về React, điều làm mình hứng thú nhất là đi sâu vào core của React là React Fiber, sau quá trình tìm hiểu nó càng làm mình cảm thấy thú vị và mình thì tính hay quên rất nhanh, để không phải đọc lại tất cả các tài liệu mà đã tìm hiểu lúc muốn hiểu lại thì mình quyết định sẽ gạch ra ý chính của tất cả các bài viết mình đã tham khảo, và mình cần một nơi để lưu trữ lại những kiến thức đó, mình đã có ý định viết blog từ khá lâu nhưng vì khá lười và hơi bận chút nên chưa thực hiện được, nhận dịp này kết hợp với việc đang tìm hiểu React vì vậy mà mình đã quyết định tạo ra Blog này. Công nghệ sử dụng là React và Gatsby. [React](https://reactjs.org/) [Gatsby](https://www.gatsbyjs.org/)
 
 # Kết thúc
-Vậy là blog đầu tiên của mình cũng đã hoàn thành, cảm thấy thật hào hứng và sớm thôi những kiến thức mà mình học được sẽ sớm được chia sẻ trên blog này!
+Vậy là blog đầu tiên của mình cũng đã hoàn thành, cảm thấy thật hào hứng và sớm thôi những kiến thức mà mình học được sẽ được chia sẻ trên blog này!
---
+++ src/pages/index1.js
@@ -0,0 +1,182 @@
+import React from "react"
+import { Link, graphql, StaticQuery } from "gatsby"
+
+import { rhythm } from "../utils/typography"
+import { formatReadingTime } from "../utils/helpers"
+
+import InfiniteScroll from "react-infinite-scroller"
+import { Spin /* , Icon */ } from "antd"
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "antd/lib/spin/style/index.css"
+// import 'antd/lib/icon/style/index.css';
+
+class BlogIndex extends React.Component {
+  postsPerLoad = 5
+
+  state = {
+    loadCount: 1,
+    loading: false,
+    hasMore: true,
+  }
+
+  handleInfiniteOnLoad = () => {
+    const total = this.props.data.allMarkdownRemark.edges.length
+    let { loadCount } = this.state
+
+    if (loadCount < Math.ceil(total / this.postsPerLoad)) {
+      this.setState({
+        loading: true,
+      })
+
+      setTimeout(
+        () =>
+          this.setState({
+            loadCount: ++loadCount,
+            loading: false,
+          }),
+        1000
+      )
+    } else {
+      this.setState({
+        hasMore: false,
+      })
+    }
+  }
+
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    const posts = data.allMarkdownRemark.edges.slice(
+      0,
+      this.postsPerLoad * this.state.loadCount
+    )
+
+    // const antdIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />
+
+    return (
+      // <StaticQuery
+      //   query={graphql(`
+      //     query {
+      //       site {
+      //         siteMetadata {
+      //           title
+      //         }
+      //       }
+      //       allMarkdownRemark(
+      //         skip: 0
+      //         limit: ${this.state.loadCount * this.postsPerLoad}
+      //         sort: { fields: [frontmatter___date], order: DESC }
+      //       ) {
+      //         edges {
+      //           node {
+      //             excerpt
+      //             fields {
+      //               slug
+      //             }
+      //             timeToRead
+      //             frontmatter {
+      //               date(formatString: "MMMM DD, YYYY")
+      //               title
+      //               description
+      //             }
+      //           }
+      //         }
+      //         totalCount
+      //       }
+      //     }
+      //   `)}
+      //   render={data => (
+          <Layout location={this.props.location} title={siteTitle}>
+            <SEO title="All posts" />
+            <Bio />
+            <InfiniteScroll
+              // initialLoad={false}
+              pageStart={0}
+              loadMore={this.handleInfiniteOnLoad}
+              hasMore={!this.state.loading && this.state.hasMore}
+            >
+              {data.allMarkdownRemark.edges.map(({ node }) => {
+                const title = node.frontmatter.title || node.fields.slug
+                return (
+                  <article key={node.fields.slug}>
+                    <header>
+                      <h3
+                        style={{
+                          marginBottom: rhythm(1 / 4),
+                        }}
+                      >
+                        <Link
+                          style={{ boxShadow: `none` }}
+                          to={node.fields.slug}
+                        >
+                          {title}
+                        </Link>
+                      </h3>
+                      <small>
+                        {node.frontmatter.date}
+                        {` • ${formatReadingTime(node.timeToRead)}`}
+                      </small>
+                    </header>
+                    <section>
+                      <p
+                        dangerouslySetInnerHTML={{
+                          __html: node.frontmatter.description || node.excerpt,
+                        }}
+                      />
+                    </section>
+                  </article>
+                )
+              })}
+            </InfiniteScroll>
+
+            {this.state.loading && this.state.hasMore && <Spin />}
+
+            {this.props.data.allMarkdownRemark.totalCount >
+              data.allMarkdownRemark.edges.length && (
+              <Link to={`/pages/${this.state.loadCount + 1}`} rel="next">
+                Loadmore →
+              </Link>
+            )}
+          </Layout>
+      //   )}
+      // />
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      skip: 0
+      limit: 20
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          timeToRead
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
